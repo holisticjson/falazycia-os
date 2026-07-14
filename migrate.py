2b23a964e486ae863fdae8bc7e1eebcc8a4ca2e7
Tomasz Holistic <tomasz@holisticjson.pl>
2026-07-14 17:11:25 +0200

    auto: update from TOMASZDUDA [2026-07-14 17:11:25]

---
+++ migrate.py
@@ -0,0 +1,5 @@
+# 🚨 SKRYPT PRZENIESIONY DO ROOT
+Ten skrypt został zastąpiony nowym, globalnym i bezpiecznym skryptem migracji na poziomie całej przestrzeni roboczej `C:\Aplikacje MVP\`.
+
+Aktualny skrypt znajdziesz i uruchomisz stąd:
+👉 **[migrate.py](file:///C:/Aplikacje%20MVP/migrate.py)**
